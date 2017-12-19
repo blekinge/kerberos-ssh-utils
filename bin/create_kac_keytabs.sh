592ed41845e42a560bac4a5b6790fb3ca2f2ea96
Asger Askov Blekinge <abr@kb.dk>
2017-12-19 10:14:27 +0100

    Updated create_kac_keytabs.sh to guess host and work for all users

--- bin/create_kac_keytabs.sh
+++ bin/create_kac_keytabs.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 # Use this file to create local keytabs for ssh login
-# Usage: ./create_kac_keytabs abr "PASSWORD"
+# Usage: ./create_kac_keytabs "PASSWORD" [username] [server]
 
-sudo apt install -y sshpass
+dpkg -s sshpass &>/dev/null || sudo apt install -y sshpass
 
-KACUSER="$1"
-PASSWORD="$2"
-sshpass -p $PASSWORD ssh $KACUSER@kac-adm-001.kac.sblokalnet > $HOME/.ssh/$KACUSER.keytab <<EOF
-	echo -e "$PASSWORD\n$PASSWORD\n" |  ipa-getkeytab --server=kac-adm-001.kac.sblokalnet --principal=$KACUSER --keytab=\$HOME/.ssh/$KACUSER.keytab --password=$PASSWORD >/dev/null
-	cat \$HOME/.ssh/$KACUSER.keytab
+#set -x
+
+PASSWORD="$1"
+KACUSER="${2:-$USER}"
+
+project_from_username="$(echo $KACUSER| grep -o '[0-9]\+')"
+PROJECT="${project_from_username:-000}"
+
+SERVER="${3:-kac-proj-$PROJECT.kac.sblokalnet}"
+
+sshpass -p "${PASSWORD}" ssh -T "${KACUSER}@${SERVER}" > $HOME/.ssh/$KACUSER.keytab <<EOF
+	echo -e "${PASSWORD}\n${PASSWORD}\n" |  ipa-getkeytab --server=kac-adm-001.kac.sblokalnet --principal=${KACUSER} --keytab=\$HOME/.ssh/${KACUSER}.keytab --password=${PASSWORD} >/dev/null
+	cat \$HOME/.ssh/${KACUSER}.keytab
 EOF
-
-
-
