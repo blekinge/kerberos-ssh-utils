8ce4bd44c06b4962c119713d54e1caa68f79e3f1
Asger Askov Blekinge <abr@kb.dk>
2018-04-26 11:15:35 +0200

    finish_create_user.sh comments turned into echo statements and more specific sss_cache clearing

--- kac_user_creation/finish_create_user.sh
+++ kac_user_creation/finish_create_user.sh
@@ -3,18 +3,27 @@
 set -e
 set -x
 
+echo "Validating this host $(hostname)"
 hostname | grep kac-abri-001 || >&2 echo 'must be executed on host kac-abri-001' && exit 1
 
+USAGE='Please use params USERNAME PROJECT'
+echo "Validating parameters $@"
 USERNAME="$1"
-[ -n ${USERNAME} ] || >&2 echo 'Please use params USERNAME PROJECT' &&  exit 1
+[ -n ${USERNAME} ] || >&2 echo "$USAGE" &&  exit 1
 PROJECT="$2"
-[ -n ${PROJECT} ] || >&2 echo 'Please use params USERNAME PROJECT' && exit 1
+[ -n ${PROJECT} ] || >&2 echo "$USAGE" && exit 1
+
 
 sudo -i <<EOF
+echo "Get Kerberos ticket for the HDFS user"
 kinit -kt /etc/security/keytabs/hdfs.headless.keytab hdfs
+echo "Reload sss_cache to ensure that ${USERNAME} is known"
+sss_cache -u ${USERNAME}
+echo "Creating HDFS homedir hdfs:///user/${USERNAME}"
 hdfs dfs -mkdir /user/${USERNAME}
 hdfs dfs -chown ${USERNAME}:${USERNAME} /user/${USERNAME}
 EOF
 
+echo "Syncing all groups (to sync $PROJECT) with Ambari. This will require Ambari *sadm Login"
 grep ${PROJECT} /etc/ambari-server/ambari-groups.csv || echo -n ",$PROJECT" >> /etc/ambari-server/ambari-groups.csv
 sudo ambari-server sync-ldap --groups=/etc/ambari-server/ambari-groups.csv
