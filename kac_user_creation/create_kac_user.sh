3b3a65193d6e7473fd9fc0fe16a6125014eb0f03
Asger Askov Blekinge <abr@kb.dk>
2018-10-02 11:52:36 +0200

    create_kac_user.sh: Exit correctly if the params cannot be read

--- kac_user_creation/create_kac_user.sh
+++ kac_user_creation/create_kac_user.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 
 set -x
+trap '[ "$?" -ne 77 ] || exit 1' ERR
 
 USAGE="create_kac_user.sh PROJECT FIRSTNAME LASTNAME USERNAME EMAIL PHONE"
 
 echo "Validating parameters $@"
 PROJECT="$1"
-[ -n "${PROJECT}" ] || >&2 echo "$USAGE" &&  exit 1
+[ -n "${PROJECT}" ] || (>&2 echo "$USAGE" &&  exit 77)
 FIRSTNAME="$2"
-[ -n "${FIRSTNAME}" ] || >&2 echo "$USAGE" &&  exit 1
+[ -n "${FIRSTNAME}" ] || (>&2 echo "$USAGE" &&  exit 77)
 LASTNAME="$3"
-[ -n "${LASTNAME}" ] || >&2 echo "$USAGE" &&  exit 1
+[ -n "${LASTNAME}" ] || (>&2 echo "$USAGE" &&  exit 77)
 USERNAME="$4"
-[ -n "${USERNAME}" ] || >&2 echo "$USAGE" &&  exit 1
+[ -n "${USERNAME}" ] || (>&2 echo "$USAGE" &&  exit 77)
 EMAIL="$5"
-[ -n "${EMAIL}" ] || >&2 echo "$USAGE" &&  exit 1
+[ -n "${EMAIL}" ] || (>&2 echo "$USAGE" &&  exit 77)
 PHONE="$6"
-[ -n "${PHONE}" ] || >&2 echo "$USAGE" &&  exit 1
+[ -n "${PHONE}" ] || (>&2 echo "$USAGE" &&  exit 77)
 
 
 echo "Validating this host $(hostname)"
-hostname | grep kac-adm-001 || >&2 echo 'must be executed on host kac-adm-001' && exit 1
+hostname | grep kac-adm-001 || (>&2 echo 'must be executed on host kac-adm-001' && exit 77)
 
 echo "Get the group from the project name '$PROJECT'"
 group=$(getent group -- "$PROJECT" | cut -d':' -f3)
