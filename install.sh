1a6b7e4722e71ea1bf71b8f13188bb340e36729b
Asger Askov Blekinge <abr@kb.dk>
2018-12-17 14:02:14 +0100

    kerberos enabled for brave and chrome

--- install.sh
+++ install.sh
@@ -14,6 +14,15 @@ diff -w "$SCRIPT_DIR/ssh/ssh_config" "$HOME/.ssh/config" | grep  '^<' && \
     echo "You must manually merge $SCRIPT_DIR/ssh/ssh_config and $HOME/.ssh/config"
 
 
+if [[ -e "/usr/share/applications/brave-browser.desktop" && ! -e ~/.local/share/applications/brave-browser.desktop ]]; then
+    echo "Installing local brave browser desktop file"
+    cp -a /usr/share/applications/brave-browser.desktop ~/.local/share/applications/brave-browser.desktop
+fi
+
+
 #Update google chrome desktop links
 insert='--auth-server-whitelist=\*\.kach\.sblokalnet,\*\.kac\.sblokalnet'
-find "$HOME/.local" 2>/dev/null | grep "chrome" | grep "\.desktop" | xargs -r -I'{}' sed -i "/'$insert'/!s|\(Exec=.*google-chrome.*\)|\1 '$insert'|g" "{}"
+
+find "$HOME/.local" 2>/dev/null | grep "chrome" | grep "\.desktop" | xargs -r -I'{}' sed -i "/$insert/!s|\(Exec=.*google-chrome.*\)|\1 $insert|g" "{}"
+
+find "$HOME/.local" 2>/dev/null | grep "brave-browser" | grep "\.desktop" | xargs -r -I'{}' sed -i "/$insert/!s|\(Exec=.*brave-browser.*\)|\1 $insert|g" "{}"
