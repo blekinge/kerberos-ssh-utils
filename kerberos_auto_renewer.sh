fd52792127eecf663ca6b159299586377ba768ae
Asger Askov Blekinge <abr@kb.dk>
2019-04-23 11:18:34 +0200

    kerberos ticket renewer

---
+++ kerberos_auto_renewer.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+( [ -e /tmp/krenew_$UID.pid ] && ps --quick-pid $(cat /tmp/krenew_$UID.pid) >/dev/null ) || ( command -v krenew >/dev/null && krenew -a -K 2 -L -b -p /tmp/krenew_$UID.pid)
