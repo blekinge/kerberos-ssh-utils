da9388f6f27dd1726c0bad260978d96be89f56b0
Asger Askov Blekinge <abr@kb.dk>
2019-04-23 11:34:24 +0200

    create_kac_user.sh do not use local user commands, only ipa commands

--- kac_user_creation/create_kac_user.sh
+++ kac_user_creation/create_kac_user.sh
@@ -24,7 +24,7 @@ echo "Validating this host $(hostname)"
 hostname | grep kac-adm-001 || (>&2 echo 'must be executed on host kac-adm-001' && exit 77)
 
 echo "Get the group from the project name '$PROJECT'"
-group=$(getent group -- "$PROJECT" | cut -d':' -f3)
+group=$(ipa group-find --group-name=p005 --posix  | grep "GID:" | cut -d':' -f2 | sed 's/ //g')
 if [ -z "$group" ]; then
     echo "you must create group with 'ipa group-add $PROJECT --gid=<GID>' before using this script"
     exit 1
@@ -32,13 +32,13 @@ fi
 echo "Found GID for project to be $group"
 
 echo "Checking if $USERNAME already exists"
-id -- "${USERNAME}" > /dev/null && (echo "User $USERNAME already exists, aborting" && exit 77)
+ipa user-find --login="${USERNAME}" --pkey-only > /dev/null && (echo "User $USERNAME already exists, aborting" && exit 77)
 
 echo "$USERNAME does not exist"
 
 echo "Find next available UID, starting from GID=$group"
 uid=$((group+1))
-while id $uid &>/dev/null; do
+while ipa user-find --uid=${uid} --pkey-only &>/dev/null; do
     uid=$((uid+1));
 done
 echo "uid=$uid have been chosen for the new user $USERNAME"
@@ -50,10 +50,10 @@ ipa user-add \
     --last="$LASTNAME" \
     --homedir="/home/${USERNAME}" \
     --phone="$PHONE" \
-    --uid=$uid \
-    --shell=/bin/bash \
-    --gidnumber=$uid \
-    --class=KacUser \
+    --uid=${uid} \
+    --shell="/bin/bash" \
+    --gidnumber=${uid} \
+    --class="KacUser" \
     --email="$EMAIL" \
     -- \
     ${USERNAME}
@@ -63,12 +63,13 @@ ipa group-add-member --user "${USERNAME}" -- "$PROJECT"
 ipa group-add-member --user "${USERNAME}" -- kacusers
 ipa group-add-member --user "${USERNAME}" -- vpnu
 
+echo "Refreshing sss-cache to get the user to exist in unix"
+sss-cache -E
+
 echo "Make user home dir /data/home/$USERNAME"
 sudo mkdir -p -- "/data/home/${USERNAME}"
 sudo cp -u -- /etc/skel/.bash* "/data/home/${USERNAME}/"
-id "$USERNAME" || sudo sss_cache -E
-# Or we could use uid here...
-sudo chown -R -- "${USERNAME}:${USERNAME}" "/data/home/${USERNAME}"
+sudo chown -R -- "${uid}:${uid}" "/data/home/${USERNAME}"
 
 INITIAL_PASSWORD=$(openssl rand -base64 25)
 echo "Set initial password $INITIAL_PASSWORD for $USERNAME"
