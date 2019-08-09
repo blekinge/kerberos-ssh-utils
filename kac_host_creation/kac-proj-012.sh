e69271abe7fd9db2128ba5cb9d664122410a345e
Asger Askov Blekinge <abr@kb.dk>
2019-08-09 14:32:00 +0200

    kac-proj-012 rsyncs headless keytabs and configurable kac-adm-001

--- kac_host_creation/kac-proj-012.sh
+++ kac_host_creation/kac-proj-012.sh
@@ -2,13 +2,17 @@
 
 host=kac-proj-012
 
+IPAHOST=kac-adm-001.kac.sblokalnet
+
 curl -s --netrc -H "X-Requested-By: ambari" "http://kac-abri-001.kach.sblokalnet:8080/api/v1/clusters/KAC/kerberos_identities?fields=*&format=CSV" | grep $host | grep -v headless > kerberos-$host.csv
 
 
-./kerberos-create.sh kac-adm-001.kac.sblokalnet kerberos-$host.csv hosts > $host-hosts.sh
+./kerberos-create.sh $IPAHOST kerberos-$host.csv hosts > $host-hosts.sh
 
-./kerberos-create.sh kac-adm-001.kac.sblokalnet kerberos-$host.csv services > $host-services.sh
+./kerberos-create.sh $IPAHOST kerberos-$host.csv services > $host-services.sh
 
-./kerberos-create.sh kac-adm-001.kac.sblokalnet kerberos-$host.csv keytabs > $host-keytabs.sh
+./kerberos-create.sh $IPAHOST kerberos-$host.csv keytabs > $host-keytabs.sh
 
-./kerberos-create.sh kac-adm-001.kac.sblokalnet kerberos-$host.csv distribute > $host-distribute.sh
+./kerberos-create.sh $IPAHOST kerberos-$host.csv distribute > $host-distribute.sh
+
+echo "ssh root@$host -- rsync -av root@$IPAHOST:/root/cfgmount/keytabs/*.headless.keytab /etc/security/keytabs/" > $host-headless.sh
