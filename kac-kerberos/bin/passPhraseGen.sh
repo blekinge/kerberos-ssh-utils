a226e29ca929b33e1f119a5bae2107d78b6640d1
Asger Askov Blekinge <abr@kb.dk>
2019-12-03 13:13:37 +0100

    Much better passphraseGen

--- kac-kerberos/bin/passPhraseGen.sh
+++ kac-kerberos/bin/passPhraseGen.sh
@@ -1,34 +1,73 @@
 #!/usr/bin/env bash
 
-wordlist="${1:-/usr/share/dict/words}"
-numWords="${2:-5}"
-numPhrases="${3:-10}"
-joinChar="${4:- }"
+SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
 
-echo #newline
-echo "$numPhrases random lines:"
-echo "-----------------"
+numWords=5
+numLines=10
+wordlist=/usr/share/dict/words
+joinChar=' '
+verbose='false'
+camelCase='false'
+
+print_usage() {
+echo "
+Usage:
+$0 [ -w numWords ] [ -l numLines ] [ -f wordlist-file ] [ -j joinChars ] [ -v ] [ -c ]
+
+Options:
+ -w <integer>     number of words in each line. Default 5
+ -l <integer>     number of lines. Default 10
+ -f <path>        the wordlist file path. Default /usr/share/dict/words
+ -j <chars>       the chars to use to join each word in a line. Default ' '
+ -v               be more verbose
+ -c               Uppercase first letter of each word
+ -h               display this help
+" 1>&2
+}
+
+while getopts 'w:l:f:j:cvh' flag; do
+  case "${flag}" in
+    w) numWords="${OPTARG}" ;;
+    l) numLines="${OPTARG}" ;;
+    f) wordlist="${OPTARG}" ;;
+    j) joinChar="${OPTARG}" ;;
+    v) verbose='true' ;;
+    c) camelCase='true' ;;
+    h) print_usage
+       exit 0 ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+[[ "$verbose" = true ]] && echo #newline
+[[ "$verbose" = true ]] && echo "$numLines random lines:"
+[[ "$verbose" = true ]] && echo "-----------------"
 
 RANGE=$(cat "$wordlist" | wc -l)
 FLOOR=0
 phrases=1
-while [ "$phrases" -le $numPhrases ]
+while [[ "$phrases" -le ${numLines} ]]
 do
     words=1
-    while [ "$words" -le $numWords ]      # Generate 10 ($numWords) random integers.
+    while [[ "$words" -le ${numWords} ]]      # Generate 10 ($numWords) random integers.
     do
         number=0   #initialize
-        while [ "$number" -le $FLOOR ] # ?
+        while [[ "$number" -le $FLOOR ]] # ?
         do
           number=$(openssl rand 4 | od -DAn)
           let "number %= $RANGE"  # Scales $number down within $RANGE.
         done
         word=$(tail -n+$number "$wordlist" | head -1)
-        echo -n "${word}${joinChar}"
+        if [[ "$camelCase" = true ]]; then
+            echo -n "${word^}${joinChar}"
+        else
+            echo -n "${word}${joinChar}"
+        fi
         let "words += 1"  # Increment count.
     done
-    echo #newline
+	[[ "$verbose" = true ]] && echo #newline
     let "phrases += 1"  # Increment count.
     echo #newline
 done
-echo "-----------------"
+[[ "$verbose" = true ]] && echo "-----------------"
